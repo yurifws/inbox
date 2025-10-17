f574259fcc1d49a032cdc2b67ecd058ddb2605ea
yurifws <yurifws@gmail.com>
2025-10-17 15:42:06 -0300

    <feat ES-45> fetching accounts from ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,34 +5,19 @@ const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
 
 
-
-class Car {
-
-    park() {
-        return 'stopped';
-    }
-    
-    drive() {
-        return 'vroom';
-    }
-
-}
-
-let car;
-
 beforeEach(() => {
-    car = new Car();
+    // Get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        });
+
+    //Use one of those accounts to deploy
+    //the contract
 });
 
-describe('Car', () => {
-
-
-    it('can park', () => {
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        const car = new Car();
-        assert.equal(car.drive(), 'vroom');
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        
     });
 });
