afdbf6583ef5d19b37496a3dd880a7087e5b1ca9
yurifws <yurifws@gmail.com>
2025-10-17 17:33:06 -0300

    <feat ES-50> verify the initial message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,6 +8,7 @@ const web3 = new Web3(ganache.provider());
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there';
 
 beforeEach(async () => {
     // Get a list of all accounts
@@ -16,12 +17,17 @@ beforeEach(async () => {
     //Use one of those accounts to deploy
     //the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there'] })
+        .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
         .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
         assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call(); 
+        assert.equal(message, INITIAL_STRING)
     });
 });
