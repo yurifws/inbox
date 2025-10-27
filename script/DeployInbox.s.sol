2cd73ea7b40205f77d2eb8099d11a0b42edbcadb
yurifws <yurifws@gmail.com>
2025-10-27 18:19:40 -0300

    <feat ES-63> fix fmt ci

--- script/DeployInbox.s.sol
+++ script/DeployInbox.s.sol
@@ -7,15 +7,15 @@ import {Inbox} from "../src/Inbox.sol";
 contract DeployInbox is Script {
     function run() external returns (Inbox) {
         string memory initialMessage = "Hello from Foundry!";
-        
+
         vm.startBroadcast();
-        
+
         Inbox inbox = new Inbox(initialMessage);
         console.log("Inbox deployed at:", address(inbox));
         console.log("Initial message:", inbox.message());
-        
+
         vm.stopBroadcast();
-        
+
         return inbox;
     }
 }
--- test/Inbox.t.sol
+++ test/Inbox.t.sol
@@ -7,39 +7,39 @@ import {Inbox} from "../src/Inbox.sol";
 contract InboxTest is Test {
     Inbox public inbox;
     string public initialMessage = "Hello World";
-    
+
     function setUp() public {
         inbox = new Inbox(initialMessage);
     }
-    
+
     function testInitialMessage() public {
         assertEq(inbox.message(), initialMessage);
     }
-    
+
     function testSetMessage() public {
         string memory newMessage = "New message";
         inbox.setMessage(newMessage);
         assertEq(inbox.message(), newMessage);
     }
-    
+
     function testSetEmptyMessage() public {
         inbox.setMessage("");
         assertEq(inbox.message(), "");
     }
-    
+
     function testSetLongMessage() public {
         string memory longMessage = "This is a very long message that we want to store in the blockchain";
         inbox.setMessage(longMessage);
         assertEq(inbox.message(), longMessage);
     }
-    
+
     function testMultipleSetMessages() public {
         inbox.setMessage("First");
         assertEq(inbox.message(), "First");
-        
+
         inbox.setMessage("Second");
         assertEq(inbox.message(), "Second");
-        
+
         inbox.setMessage("Third");
         assertEq(inbox.message(), "Third");
     }
